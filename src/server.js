602906544419e39f672f9ef0d7297a42d375ba5a
AtifAli313 <atifvision313@gmail.com>
2026-03-23 08:59:00 +0500

    Initial backend commit

--- src/server.js
+++ src/server.js
@@ -2,10 +2,13 @@ require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./config/db");
 
+// Connect to Database
 connectDB();
 
+// Start server on Render
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+module.exports = app;
